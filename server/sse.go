2e4af4cf0c6dd36013aeb725f547ad4963c2155d
Karn <karngyan@gmail.com>
2025-04-22 18:17:36 +0530

    fix sse shutdown (#128)

--- server/sse.go
+++ server/sse.go
@@ -321,6 +321,8 @@ func (s *SSEServer) handleSSE(w http.ResponseWriter, r *http.Request) {
 		case <-r.Context().Done():
 			close(session.done)
 			return
+		case <-session.done:
+			return
 		}
 	}
 }
